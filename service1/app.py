33d5d09e3ccd3fdfc84b5683a444e0e0e6763abb
Ajithkumar <ajithkumar.ak95@gmail.com>
2025-02-19 13:50:30 +0530

    added some services

---
+++ service1/app.py
@@ -0,0 +1,5 @@
+import time
+
+while True:
+    print("I am here at service 1")
+    time.sleep(5)
---
+++ service2/app.py
@@ -0,0 +1,5 @@
+import time
+
+while True:
+    print("I am here at service 2")
+    time.sleep(5)
